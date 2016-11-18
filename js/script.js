c0851b5a4bedbd79469befaeaabdc8359868a19c
gooddha <Gooddha11>
2016-11-18 13:53:35 +0300

    Не отменяется действие по умолчанию

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var link = document.querySelector(".booking-form-container");
+var modal = document.querySelector(".modal-content");
+
+link.addEventlistener("click", function(event)) {
+  event.preventDefault();
+}
