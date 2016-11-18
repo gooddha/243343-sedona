3dc103ac4146f50bf831298f35f3a2297f38633e
gooddha <Gooddha11>
2016-11-18 13:51:54 +0300

    Revert "Начал делать анимацию для формы"
    This reverts commit d08e4e82ff11d26970dc39ae138e5c1a5d216ccd.

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var link = document.querySelector(".booking-form-container");
-var modal = document.querySelector(".modal-content");
-
-link.addEventlistener("click", function(event)) {
-  event.preventDefault();
-}
