070dc659dde8fa07feef93b81e5bbc028541861e
gooddha <Gooddha11>
2016-11-19 15:53:55 +0300

    Заканчиваю главную страницу
    Сделал интерактивную карту и кастомизировал состояния svg иконки
    html-academy

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ var modal = document.querySelector(".booking-form-container");
 link.addEventListener("click", function(event) {
   event.preventDefault();
   modal.classList.toggle("modal-content-show");
+  // modal.classList.toggle("modal-content-z-index");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    modal.classList.remove("modal-content-show");
+    // modal.classList.toggle("modal-content-z-index");
+  }
 });
